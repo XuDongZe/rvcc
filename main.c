d0ca68352bdab4b68a9269d94b2d36eacba98955
dongze <1146520765@qq.com>
2022-12-01 21:23:35 +0800

    update gitignore

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+        return 1;
+    }
+
+    char *p = argv[1];
+
+    printf("  .global main\n");
+    printf("main:\n");
+    printf("  li a0, %ld\n", strtol(p, &p, 10));
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("  addi a0, a0, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-')
+        {
+            p++;
+            printf(" addi a0, a0, -%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        fprintf(stderr, "unexpected character: '%c'\n", *p);
+        return 1;
+    }
+    printf("  ret\n");
+    return 0;
+}
