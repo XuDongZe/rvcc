7653d6387d65e38e42f75ca24764060e70a63111
dongze <1146520765@qq.com>
2022-12-01 21:16:20 +0800

    [2]支持+ -运算符

--- main.c
+++ main.c
@@ -3,14 +3,34 @@
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
         return 1;
     }
-    
+
+    char *p = argv[1];
+
     printf("  .global main\n");
     printf("main:\n");
-    printf("  li a0, %d\n", atoi(argv[1]));
+    printf("  li a0, %ld\n", strtol(p, &p, 10));
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("  addi a0, a0, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-')
+        {
+            p++;
+            printf(" addi a0, a0, -%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        fprintf(stderr, "unexpected character: '%c'\n", *p);
+        return 1;
+    }
     printf("  ret\n");
     return 0;
 }
