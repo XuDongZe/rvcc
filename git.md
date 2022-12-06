45b880f42ac1aa5168b0300cfd19f7b211a09b37
dongze <1146520765@qq.com>
2022-12-06 13:55:01 +0800

    [5] 支持+ / ()

---
+++ git.md
@@ -0,0 +1,21 @@
+git init
+
+git add .
+git commit -m <msg>
+git push origin main
+
+git rm -f cached .
+
+git config user.name
+git config user.email
+git config --global user.name "dongze"
+git config --global user.email "xxx"
+
+git config --list
+git config --list | grep url
+
+git clone
+git pullc
+git branch
+git status
+git log
--- main.c
+++ main.c
@@ -4,15 +4,55 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stdbool.h>
+#include <assert.h>
 
 char *currentInput;
 
 typedef enum
 {
-    TOK_OP, // 操作符号
+    TOK_PUNCT, // 操作符号
     TOK_NUM,
     TOK_EOF, // 文件终止符
 } TokenKind;
+
+typedef enum
+{
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+struct Node {
+    NodeKind kind;
+    struct Node *lhs; // left-hand side
+    struct Node *rhs; // right-hand side
+    int val;          // 存储ND_NUM的值
+};
+
+static Node *newNode(NodeKind kind)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+static Node *newBinary(NodeKind kind, Node *l, Node *r)
+{
+    Node *node = newNode(kind);
+    node->lhs = l;
+    node->rhs = r;
+    return node;
+}
+
+static Node *newNum(int val)
+{
+    Node *node = newNode(ND_NUM);
+    node->val = val;
+    return node;
+}
 
 static void error(char *fmt, ...)
 {
@@ -70,6 +110,15 @@ static bool equal(Token *tok, char *str)
     return memcmp(tok->loc, str, tok->len) == 0 && str[tok->len] == '\0';
 }
 
+static Token *skip(Token *tok, char *str)
+{
+    if (!equal(tok, str))
+    {
+        errorAt(tok->loc, str);
+    }
+    return tok->next;
+}
+
 static long getNumber(Token *tok)
 {
     if (tok->kind != TOK_NUM)
@@ -77,6 +126,144 @@ static long getNumber(Token *tok)
         errorAt(tok->loc, "expect a number");
     }
     return tok->val;
+}
+
+static Node *expr(Token **rest, Token *tok);
+static Node *mul(Token **rest, Token *tok);
+static Node *primary(Token **rest, Token *tok);
+
+// primary = "(" expr ")" | num
+static Node *primary(Token **rest, Token *tok)
+{
+    // "(" expr ")"
+    if (equal(tok, "("))
+    {
+        Node *node = expr(&tok, tok->next);
+        *rest = skip(tok, ")");
+        return node;
+    }
+    // num
+    if (tok->kind == TOK_NUM)
+    {
+        Node *node = newNum(tok->val);
+        *rest = tok->next;
+        return node;
+    }
+    errorAt(tok->loc, "expected an expression");
+}
+
+// mul = primary ("*" primary | "/" primary)*
+static Node *mul(Token **rest, Token *tok)
+{
+    // primary
+    Node *node = primary(&tok, tok);
+    // ("*" primary | "/" primary)*
+    while (true)
+    {
+        // "*" primary
+        if (equal(tok, "*"))
+        {
+            node = newBinary(ND_MUL, node, primary(&tok, tok->next));
+            continue;
+        }
+        if (equal(tok, "/"))
+        {
+            node = newBinary(ND_DIV, node, primary(&tok, tok->next));
+            continue;
+        }
+
+        *rest = tok;
+        return node;
+    }
+}
+
+// expr = mul ("+" mul | "-" mul)*
+static Node *expr(Token **rest, Token *tok)
+{
+    // mul
+    Node *node = mul(&tok, tok);
+    while (true)
+    {
+        // "+" mul
+        if (equal(tok, "+"))
+        {
+            node = newBinary(ND_ADD, node, mul(&tok, tok->next));
+            continue;
+            ;
+        }
+        // "-" mul
+        if (equal(tok, "-"))
+        {
+            node = newBinary(ND_SUB, node, mul(&tok, tok->next));
+            continue;
+        }
+
+        *rest = tok;
+        return node;
+    }
+}
+
+// 语义分析与代码生成
+static int depth;
+
+// 压栈，结果临时压入栈中保存
+// sp为栈顶指针 stack pointer，栈反向向下增长。64位下，一个单位8个字节。
+// 不使用寄存器，因为需要存储的值的数量是变化的
+
+static void push(void)
+{
+    // 调整sp
+    printf(" addi sp, sp, -8\n");
+    // 将a0压入栈
+    // save double, from a0, to sp[0]
+    printf(" sd a0, 0(sp)\n");
+    // 记录当前栈深度
+    depth++;
+}
+
+static void pop(char *reg)
+{
+    // load double,
+    printf(" ld %s, 0(sp)\n", reg);
+    printf(" addi sp, sp, 8\n");
+    depth--;
+}
+
+static void genExpr(Node *node)
+{
+    if (node->kind == ND_NUM)
+    {
+        printf(" li a0, %d\n", node->val);
+        return;
+    }
+
+    genExpr(node->rhs);
+    push();
+    genExpr(node->lhs);
+    pop("a1");
+
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf(" add a0, a0, a1\n");
+        return;
+    case ND_SUB:
+        printf(" sub a0, a0, a1\n");
+        return;
+    case ND_MUL:
+        printf(" mul a0, a0, a1\n");
+        return;
+    case ND_DIV:
+        printf(" div a0, a0, a1\n");
+        return;
+    default:
+        error("invalid expression");
+        return;
+    }
+}
+
+static bool isPunct(char p) {
+    return p == '+' || p == '-' || p == '*' || p == '/' || p == '(' || p == ')';
 }
 
 static Token *tokenize(char *p)
@@ -101,9 +288,9 @@ static Token *tokenize(char *p)
             cur = cur->next;
             continue;
         }
-        if (*p == '+' || *p == '-')
+        if (isPunct(*p))
         {
-            Token *tok = newToken(TOK_OP, p, p + 1);
+            Token *tok = newToken(TOK_PUNCT, p, p + 1);
             cur->next = tok;
             cur = cur->next;
             p++;
@@ -115,7 +302,7 @@ static Token *tokenize(char *p)
     return head.next;
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
@@ -124,31 +311,24 @@ int main(int argc, char const *argv[])
     }
 
     currentInput = argv[1];
-    Token *tok = tokenize(argv[1]);
+    // currentInput = "1-8/(2*2)+3*6";
+    Token *tok = tokenize(currentInput);
 
+    Node *node = expr(&tok, tok);
+    if (tok->kind != TOK_EOF) {
+        errorAt(tok->loc, "extra token: %d", tok->kind);
+    }
+
+    // 声明一个全局main段，同时也是程序入口段
     printf("  .global main\n");
     printf("main:\n");
-    printf("  li a0, %ld\n", getNumber(tok));
-    tok = tok->next;
 
-    while (tok->kind != TOK_EOF)
-    {
-        if (equal(tok, "+"))
-        {
-            tok = tok->next;
-            printf("  addi a0, a0, %ld\n", getNumber(tok));
-            tok = tok->next;
-            continue;
-        }
-        if (equal(tok, "-"))
-        {
-            tok = tok->next;
-            printf(" addi a0, a0, -%ld\n", getNumber(tok));
-            tok = tok->next;
-            continue;
-        }
-        errorAt(tok->loc, "unexpected tok: %s", tok->loc);
-    }
-    printf("  ret\n");
+    // 便利AST树生成汇编代码
+    genExpr(node);
+    
+    printf(" ret\n");
+
+    // 如果stack未清空 报错
+    assert(depth == 0);
     return 0;
 }
